e0886b4c76a703bca7dce2adcc899d9a53373758
HaydenTLM <46750343+HaydenTLM@users.noreply.github.com>
2019-03-26 00:20:28 -0400

    Delete CurrentUser.java

--- CurrentUser.java
+++
@@ -1,8 +0,0 @@
-public class CurrentUser {
-  UserClass user;
-
-  public CurrentUser(UserClass us) {user = us;}
-
-  public void setCurrentUser(UserClass us) {user = us;}
-  public UserClass getCurrentUser() {return user;}
-}
